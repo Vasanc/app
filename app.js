666b5a4efcad28e3bf7b0ad4d60a11b130296d77
Vasanc <vasanaids@smvec.ac.in>
2022-08-31 10:09:01 +0530

    connected to mongo

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ app.set("view engine", "ejs")
 app.use(express.static('views'))
 app.use(express.static('assets'))
 
+const mongoose = require('mongoose')
+
+const url = `mongodb+srv://sample_user:<password>@my-sample-cluster-b3ugy.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
+
 
 app.listen(6060,()=>
 {
